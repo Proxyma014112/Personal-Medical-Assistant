edae9e5afe38102796e7fc79674ad2cb4d6c3553
2024ashikul <2024ashikul@gmail.com>
2026-06-29 20:11:55 +0600

    Fix audio in phone

--- app.py
+++ app.py
@@ -685,7 +685,7 @@ else:
         if is_bangla else
         "Type your symptoms or question, or use the microphone..."
     )
-    prompt = st.chat_input(placeholder, accept_audio=True)
+    prompt = st.chat_input(placeholder, accept_audio=True,key=st.session_state.input_key)
 
     if prompt:
         user_input = ""
